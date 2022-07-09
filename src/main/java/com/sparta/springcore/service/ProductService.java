d6f009bc271969bba812d9e33ea3769ba3188348
hyeonorof <hyeonorof@gmail.com>
2022-07-09 23:13:51 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.sparta.springcore.service;
+
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(ProductRequestDto requestDto) {
+        // 요청받은 DTO로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+
+        productRepository.save(product);
+
+        return product;
+    }
+
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        if (requestDto.getMyprice() <= 0) {
+            throw new RuntimeException("희망 최저가는 0원 이상으로 설정해 주세요.");
+        }
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
+        if(product == null) {
+            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
+        }
+
+        int myprice = requestDto.getMyprice();
+        product.setMyprice(myprice);
+        productRepository.save(product);
+
+        return product;
+    }
+
+    public List<Product> getProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products;
+    }
+}
